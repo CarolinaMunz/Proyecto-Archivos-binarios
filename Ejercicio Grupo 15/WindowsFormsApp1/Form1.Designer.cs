62f6f76423f4fa8fcdc951f7911a757ea434ca85
Carolina Stefany Münz <119470260+CarolinaMunz@users.noreply.github.com>
2025-02-03 23:33:11 -0300

    Add files via upload

---
+++ Ejercicio Grupo 15/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txtNumeroTelefono = new System.Windows.Forms.TextBox();
+            this.lbAlumnos = new System.Windows.Forms.ListBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPathFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Salmon;
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de Alumnos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DNI:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Número de teléfono:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Salmon;
+            this.label5.Location = new System.Drawing.Point(509, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Alumnos registrados";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.MistyRose;
+            this.txtNombre.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(97, 176);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(306, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.BackColor = System.Drawing.Color.MistyRose;
+            this.txtDNI.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDNI.Location = new System.Drawing.Point(73, 219);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(330, 26);
+            this.txtDNI.TabIndex = 6;
+            // 
+            // txtNumeroTelefono
+            // 
+            this.txtNumeroTelefono.BackColor = System.Drawing.Color.MistyRose;
+            this.txtNumeroTelefono.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroTelefono.Location = new System.Drawing.Point(167, 264);
+            this.txtNumeroTelefono.Name = "txtNumeroTelefono";
+            this.txtNumeroTelefono.Size = new System.Drawing.Size(236, 26);
+            this.txtNumeroTelefono.TabIndex = 7;
+            // 
+            // lbAlumnos
+            // 
+            this.lbAlumnos.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.lbAlumnos.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.lbAlumnos.FormattingEnabled = true;
+            this.lbAlumnos.ItemHeight = 19;
+            this.lbAlumnos.Location = new System.Drawing.Point(447, 71);
+            this.lbAlumnos.Name = "lbAlumnos";
+            this.lbAlumnos.Size = new System.Drawing.Size(342, 327);
+            this.lbAlumnos.TabIndex = 8;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnGuardar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnGuardar.Location = new System.Drawing.Point(27, 314);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(122, 52);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar datos";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCargar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCargar.Location = new System.Drawing.Point(155, 314);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(117, 52);
+            this.btnCargar.TabIndex = 10;
+            this.btnCargar.Text = "Cargar datos";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnLimpiar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLimpiar.Location = new System.Drawing.Point(278, 314);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(125, 52);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar lista";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCrear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCrear.Location = new System.Drawing.Point(27, 71);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(142, 52);
+            this.btnCrear.TabIndex = 12;
+            this.btnCrear.Text = "Crear archivo";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSeleccionar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSeleccionar.Location = new System.Drawing.Point(203, 71);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(179, 52);
+            this.btnSeleccionar.TabIndex = 13;
+            this.btnSeleccionar.Text = "Seleccionar archivo";
+            this.btnSeleccionar.UseVisualStyleBackColor = false;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 19);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Ruta del archivo:";
+            // 
+            // txtPathFile
+            // 
+            this.txtPathFile.BackColor = System.Drawing.Color.MistyRose;
+            this.txtPathFile.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPathFile.Location = new System.Drawing.Point(153, 135);
+            this.txtPathFile.Name = "txtPathFile";
+            this.txtPathFile.Size = new System.Drawing.Size(250, 26);
+            this.txtPathFile.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPathFile);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lbAlumnos);
+            this.Controls.Add(this.txtNumeroTelefono);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de Alumnos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.TextBox txtNumeroTelefono;
+        private System.Windows.Forms.ListBox lbAlumnos;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPathFile;
+    }
+}
+
---
+++ Ejercicio Grupo 15/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Archivos Binarios (*.bin)|*.bin";
+                    saveFileDialog.Title = "Crea un nuevo archivo";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        using (FileStream fs = File.Create(saveFileDialog.FileName))
+                        {
+                            //Archivo cerrado
+                        }
+                        MessageBox.Show("Archivo creado exitosamente.");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Error al seleccionar el archivo: {ex.Message}");
+            }
+
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using(OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Archivos Binarios (*.bin)|*.bin";
+                    openFileDialog.Title = "Selecciona un archivo existente";
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        txtPathFile.Text = openFileDialog.FileName;
+                        MessageBox.Show("Archivo seleccionado exitosamente.");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Error al seleccionar el archivo: {ex.Message}");
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPathFile.Text))
+                {
+                    MessageBox.Show("Por favor, selecciona un archivo antes de guardar los datos.");
+                    return;
+                }
+
+                string nombre = txtNombre.Text;
+
+                if ( !int.TryParse(txtDNI.Text, out int dni) || !int.TryParse(txtNumeroTelefono.Text, out int numTel))
+                {
+                    MessageBox.Show("Por favor, complete el DNI y el nmero de teléfono con números enteros.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(nombre))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos.");
+                    return;
+                }
+                
+                using (BinaryWriter writer = new BinaryWriter(File.Open(txtPathFile.Text, FileMode.Append)))
+                {
+                    writer.Write(nombre);
+                    writer.Write(dni);
+                    writer.Write(numTel);
+                }
+
+                MessageBox.Show("Datos guardados exitosamente.");
+
+                txtNombre.Clear();
+                txtDNI.Clear();
+                txtNumeroTelefono.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar los datos: {ex.Message}");
+            }
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPathFile.Text))
+                {
+                    MessageBox.Show("Por favor, selecciona un archivo antes de leer los datos.");
+                    return;
+                }
+
+                if (!File.Exists(txtPathFile.Text))
+                {
+                    MessageBox.Show("El archivo especificado no existe.");
+                    return;
+                }
+
+                using (BinaryReader reader = new BinaryReader(File.Open(txtPathFile.Text, FileMode.Open)))
+                {
+                    while (reader.BaseStream.Position != reader.BaseStream.Length) 
+                    {
+                        string nombre = reader.ReadString();
+                        int dni = reader.ReadInt32();
+                        int numTel = reader.ReadInt32();
+
+                        lbAlumnos.Items.Add($"Nombre: {nombre}, DNI: {dni}, Teléfono: {numTel}");
+                    }
+                }
+
+                MessageBox.Show("Datos leídos exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al leer los datos: {ex.Message}");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lbAlumnos.Items.Clear();
+        }
+    }
+}
